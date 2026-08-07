5663178da621fc55b2990c583eb411c1950f68a2
Vansh Sood <25641301+Vansh983@users.noreply.github.com>
2026-08-06 23:33:56 -0400

    fix: remove duplicate unescaped I'm that failed lint on build

--- pages/index.tsx
+++ pages/index.tsx
@@ -216,7 +216,7 @@ const Home: NextPage = () => {
               </span>{" "}
             </h1> */}
             <p className='text-3xl text-gray-400 mt-4'>
-              I'm I&apos;m a 3rd year Computer Science student at Dalhousie
+              I&apos;m a 3rd year Computer Science student at Dalhousie
               University. I barely manage to attend classes and try to promote
               freelancing/ solopreneurship among students. I also run a{" "}
               <span className='text-sky-600'>software company</span>, build{" "}
